ff32ccc5f8d24ce0923ff151f9fa6c158d388371
kicknback <joshua.borreli@gmail.com>
2021-07-05 12:15:41 -0500

    Adding more code wars exercises

--- codewars-exercises.py
+++ codewars-exercises.py
@@ -55,4 +55,53 @@ def find_it(seq):
             return i
 
 
-print(find_it([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]))
+# print(find_it([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]))
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# The new "Avengers" movie has just been released!
+# There are a lot of people at the cinema box office standing in a huge line.
+# Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+#
+# Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+#
+# Can Vasya sell a ticket to every person and give change
+# if he initially has no money and sells the tickets strictly in the order people queue?
+#
+# Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment.
+# Otherwise return NO.
+#
+# Examples:
+# tickets([25, 25, 50]) # => YES
+# tickets([25, 100]) # => NO. Vasya will not have enough money to give change to 100 dollars
+# tickets([25, 25, 50, 50, 100]) # => NO. Vasya will not have the right bills to give 75 dollars of change
+# (you can't make two bills of 25 from one of 50)
+
+def tickets(people):
+    total = 0
+    for ticket in people:
+        if ticket == 25:
+            total += ticket
+        else:
+            change = ticket - 25
+            if change > total:
+                return "NO"
+            else:
+                total -= change
+    return "YES"
+
+
+print(tickets([25, 25, 50, 50, 100]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
