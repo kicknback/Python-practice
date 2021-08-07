71b5aec154d7a3f6a0a8124224fca6b43e093f05
kicknback <joshua.borreli@gmail.com>
2021-08-06 23:46:10 -0500

    Beginning work on range extraction exercise

--- codewars-exercises.py
+++ codewars-exercises.py
@@ -309,6 +309,11 @@ def make_readable(seconds):
 # returns "-6,-3-1,3-5,7-11,14,15,17-20"
 
 def solution(args):
+    range_string = ""
+    for i, v in enumerate(args):
+        last_value = v
+        if i == 0:
+            range_string += v
 
 
 
